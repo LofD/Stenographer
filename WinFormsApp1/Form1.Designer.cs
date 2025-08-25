65247edd0ea9075a55944fcf3ff7ad0b7fc01186
Gogovych Sergii <gogovych.sergii@gmail.com>
2025-08-25 20:45:10 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            notifyIcon1 = new NotifyIcon(components);
+            progressBar1 = new ProgressBar();
+            button3 = new Button();
+            label1 = new Label();
+            settingsBox = new GroupBox();
+            modelsSelect = new ComboBox();
+            saveSettingsButton = new Button();
+            settingsBox.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(307, 11);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(388, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(400, 292);
+            textBox1.TabIndex = 2;
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.Visible = true;
+            notifyIcon1.DoubleClick += notifyIcon1_DoubleClick;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(12, 41);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(370, 23);
+            progressBar1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(388, 310);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 4;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(388, 336);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 5;
+            label1.Text = "label1";
+            // 
+            // settingsBox
+            // 
+            settingsBox.Controls.Add(saveSettingsButton);
+            settingsBox.Controls.Add(modelsSelect);
+            settingsBox.Location = new Point(12, 151);
+            settingsBox.Name = "settingsBox";
+            settingsBox.Size = new Size(306, 287);
+            settingsBox.TabIndex = 7;
+            settingsBox.TabStop = false;
+            settingsBox.Text = "Settings";
+            // 
+            // modelsSelect
+            // 
+            modelsSelect.DropDownStyle = ComboBoxStyle.DropDownList;
+            modelsSelect.FormattingEnabled = true;
+            modelsSelect.Location = new Point(6, 22);
+            modelsSelect.Name = "modelsSelect";
+            modelsSelect.Size = new Size(294, 23);
+            modelsSelect.TabIndex = 7;
+            // 
+            // saveSettingsButton
+            // 
+            saveSettingsButton.Location = new Point(225, 258);
+            saveSettingsButton.Name = "saveSettingsButton";
+            saveSettingsButton.Size = new Size(75, 23);
+            saveSettingsButton.TabIndex = 8;
+            saveSettingsButton.Text = "Save";
+            saveSettingsButton.UseVisualStyleBackColor = true;
+            saveSettingsButton.Click += saveSettingsButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(settingsBox);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(progressBar1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosed += Form1_FormClosed;
+            Load += Form1_Load;
+            Resize += Form1_Resize;
+            settingsBox.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private NotifyIcon notifyIcon1;
+        private ProgressBar progressBar1;
+        private Button button3;
+        private Label label1;
+        private GroupBox settingsBox;
+        private ComboBox modelsSelect;
+        private Button saveSettingsButton;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,202 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Gui;
+using NAudio.Wave;
+using Newtonsoft.Json.Linq;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Security;
+using Vosk;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const int HOTKEY_ID = 9000;
+        private const uint MOD_NONE = 0x0000;
+        private const uint VK_F9 = 0x78;
+        private const int WM_HOTKEY = 0x0312;
+
+        private Model _model;
+        private VoskRecognizer _recognizer;
+        private WaveInEvent _waveIn;
+        private WaveFileWriter _writer;
+
+        private void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            // TODO: remove this line if you don't want to save the audio to a file
+            _writer.Write(e.Buffer, 0, e.BytesRecorded);
+
+
+            if (_recognizer.AcceptWaveform(e.Buffer, e.BytesRecorded))
+            {
+                string json = _recognizer.Result();
+                string text = JObject.Parse(json)["text"]?.ToString();
+                AppendText("👂 " + text + "\r\n");
+            }
+        }
+
+        private void AppendText(string text)
+        {
+            if (textBox1.InvokeRequired)
+            {
+                textBox1.Invoke(new Action(() => textBox1.AppendText(text)));
+            }
+            else
+            {
+                textBox1.AppendText(text);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [HandleProcessCorruptedStateExceptions]
+        protected async void initVoskModel()
+        {
+            string modelName = Properties.Settings.Default.ModelName;
+            try
+            {
+                Vosk.Vosk.SetLogLevel(0);
+
+                //_model = new Model("big-model"); // путь к папке с моделью
+
+
+                progressBar1.Style = ProgressBarStyle.Marquee;
+                await Task.Run(() => _model = new Model(modelName));
+
+                
+
+                progressBar1.Style = ProgressBarStyle.Blocks;
+
+                _recognizer = new VoskRecognizer(_model, 16000.0f);
+
+                _waveIn = new WaveInEvent
+                {
+                    DeviceNumber = 0,
+                    WaveFormat = new WaveFormat(16000, 1),
+                    BufferMilliseconds = 50
+                };
+
+                _waveIn.DataAvailable += OnDataAvailable;
+                _writer = new WaveFileWriter("test.wav", _waveIn.WaveFormat);
+                _waveIn.StartRecording();
+
+                AppendText("Началось распознавание...\r\n");
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+            catch  (AccessViolationException ex)
+            {
+                MessageBox.Show("Ошибка доступа к модели: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY && m.WParam.ToInt32() == HOTKEY_ID)
+            {
+                // Здесь твое действие при нажатии F9
+                MessageBox.Show("F9 нажата!");
+            }
+            base.WndProc(ref m);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            initVoskModel();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _waveIn?.StopRecording();
+            _waveIn?.Dispose();
+            _recognizer?.Dispose();
+            _model?.Dispose();
+            _writer?.Dispose();
+
+            Clipboard.SetText(textBox1.Text);
+
+            AppendText("Распознавание остановлено.\r\n");
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RegisterHotKey(this.Handle, HOTKEY_ID, MOD_NONE, VK_F9);
+
+
+            string path = @".\models";
+            modelsSelect.Items.Add("--");
+            modelsSelect.SelectedIndex = 0;
+            if (Directory.Exists(path))
+            {
+                string[] folders = Directory.GetDirectories(path);
+
+                foreach (string folder in folders)
+                {
+                    modelsSelect.Items.Add(folder);
+                }
+            }
+
+            string modelName = Properties.Settings.Default.ModelName;
+            int selectedIndex = modelsSelect.Items.IndexOf(modelName);
+            if (selectedIndex != -1)
+            {
+                modelsSelect.SelectedIndex = selectedIndex;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+                //notifyIcon1.ShowBalloonTip(1000, "Программа свернута", "Работает в фоновом режиме", ToolTipIcon.Info);
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, HOTKEY_ID);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //create enumerator
+            var enumerator = new MMDeviceEnumerator();
+            //cycle through all audio devices
+            for (int i = 0; i < WaveIn.DeviceCount; i++)
+                label1.Text += i.ToString() + ": " + (enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active)[i]);
+            //clean up
+            enumerator.Dispose();
+        }
+
+        private void saveSettingsButton_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.ModelName = modelsSelect.SelectedItem?.ToString() ?? "--";
+            Properties.Settings.Default.Save();
+        }
+    }
+}
